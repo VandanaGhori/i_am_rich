406f33f9348646f7c0de73ad31e830ab0988dc5e
Vandana Ghori <vandanaghori19@gmail.com>
2022-08-09 19:43:26 -0400

    initial setup commit

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("I am rich"),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+      ),
+    ),
+  );
+}
